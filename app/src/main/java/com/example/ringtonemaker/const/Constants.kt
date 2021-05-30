0ebe5014a0126d29d5e41300f9893b2dc426fc1f
andrey marin <anmarin86@gmail.com>
2021-05-30 12:48:45 +0300

    fix permissions in FinalFragment

--- app/src/main/java/com/example/ringtonemaker/const/Constants.kt
+++ app/src/main/java/com/example/ringtonemaker/const/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val VISIBLE_ITEM_COUNT = 2
     const val DEFAULT_INDEX = 0
     const val READ_WRITE_PERMISSION_REQUEST_CODE = 2222
+    const val CODE_WRITE_SETTINGS_PERMISSION = 66
 
 }
--- app/src/main/java/com/example/ringtonemaker/ui/FinalFragment.kt
+++ app/src/main/java/com/example/ringtonemaker/ui/FinalFragment.kt
@@ -1,6 +1,10 @@
 package com.example.ringtonemaker.ui
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
@@ -9,59 +13,111 @@ import android.provider.Settings
 import android.view.View
 import androidx.activity.addCallback
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.ringtonemaker.R
-
 import com.example.ringtonemaker.ViewBindingFragment
+import com.example.ringtonemaker.const.Constants
 import com.example.ringtonemaker.databinding.FragmentFinalBinding
 import com.example.ringtonemaker.utils.createSnackBar
 
-class FinalFragment: ViewBindingFragment<FragmentFinalBinding>(FragmentFinalBinding::inflate) {
+
+class FinalFragment : ViewBindingFragment<FragmentFinalBinding>(FragmentFinalBinding::inflate) {
 
     private val args: FinalFragmentArgs by navArgs()
+    private lateinit var ringtoneName: String
+    private lateinit var ringtonePath: String
+    private lateinit var ringtoneUri: Uri
+
+
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        ringtoneName = args.ringtoneName
+        ringtonePath = args.ringtonePath
+        ringtoneUri = args.ringtoneUri
         super.onViewCreated(view, savedInstanceState)
         binding.buttonFinalFragmentPlayRingtone.setOnClickListener {
-            val action = FinalFragmentDirections.actionFinalFragmentToExoPlayerBottomDialog(args.ringtonePath)
+            val action =
+                FinalFragmentDirections.actionFinalFragmentToExoPlayerBottomDialog(ringtonePath)
             findNavController().navigate(action)
         }
 
         binding.buttonFinalFragmentSetAsACallMelody.setOnClickListener {
-            setRingtone(args.ringtoneUri)
+            setRingtone(requireActivity(), ringtoneUri)
         }
 
-        binding.textViewFinalFragmentRingtoneName.text = args.ringtoneName
-        binding.textViewFinalFragmentRingtonePath.text = args.ringtonePath
+        binding.textViewFinalFragmentRingtoneName.text = ringtoneName
+        binding.textViewFinalFragmentRingtonePath.text = ringtonePath
 
-        requireActivity().onBackPressedDispatcher.addCallback{
+        requireActivity().onBackPressedDispatcher.addCallback {
             findNavController().navigate(R.id.action_finalFragment_to_backPressedDialog2)
             isEnabled = true
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
-    fun setRingtone(uri: Uri?){
-        if (Settings.System.canWrite(requireContext())){
-            createSnackBar(getString(R.string.ringtoneSettedAsPhoneMelody))
-            RingtoneManager.setActualDefaultRingtoneUri(
-                requireContext(),
-                RingtoneManager.TYPE_RINGTONE,
-                uri
-            )
-        } else {
-            openAndroidPermissionsMenu()
-            createSnackBar(getString(R.string.ringtoneSetAsMelodyError))
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Constants.CODE_WRITE_SETTINGS_PERMISSION && Settings.System.canWrite(requireContext())){
+            setActualDefaultRingtone(requireContext(), ringtoneUri)
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun openAndroidPermissionsMenu() {
+    private fun openAndroidPermissionsMenu(context: Activity) {
         val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
-        intent.data = Uri.parse("package:" + requireActivity().packageName)
-        startActivity(intent)
+        intent.data = Uri.parse("package:" + context.packageName)
+        context.startActivityForResult(intent, Constants.CODE_WRITE_SETTINGS_PERMISSION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.CODE_WRITE_SETTINGS_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setActualDefaultRingtone(requireContext(), ringtoneUri)
+        }
+    }
+
+    private fun setRingtone(context: Activity, ringtoneUri: Uri) {
+        val permission: Boolean = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Settings.System.canWrite(context)
+        } else {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.WRITE_SETTINGS
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+        if (permission) {
+            try {
+                setActualDefaultRingtone(requireContext(), ringtoneUri)
+                createSnackBar(getString(R.string.ringtoneSettedAsPhoneMelody))
+            } catch (e: Exception) {
+                createSnackBar(getString(R.string.ringtoneSetAsMelodyError))
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                openAndroidPermissionsMenu(context)
+            } else {
+                ActivityCompat.requestPermissions(
+                    context,
+                    arrayOf(Manifest.permission.WRITE_SETTINGS),
+                    Constants.CODE_WRITE_SETTINGS_PERMISSION
+                )
+            }
+        }
+    }
+
+    private fun setActualDefaultRingtone(context: Context, ringtoneUri: Uri){
+        RingtoneManager.setActualDefaultRingtoneUri(
+            context,
+            RingtoneManager.TYPE_RINGTONE,
+            ringtoneUri
+        )
     }
 
 }
